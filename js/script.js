0df0f7bf4d12d16011a7e4c65cd850b6092e9202
Place4you <66410030+Place4you@users.noreply.github.com>
2023-08-19 14:53:21 +0500

    javascript

---
+++ js/script.js
@@ -0,0 +1,105 @@
+
+
+    //textlitte 
+    var  timeline= gsap.timeline();
+    timeline
+        .from("#lowest",{
+            opacity:0,
+            delay:0,
+            onStart:function(){
+                $('#lowest').textillate({
+                     in: { 
+                            effect: 'fadeInUp',
+                            callback:function(){
+                                $('#lowest').textillate('out')
+                                 }
+                         },
+                     out: { effect: 'fadeOutUp' } 
+                    });
+                 }})
+    
+        .from("#second-lowest",{
+            opacity:0,
+            delay:1,
+            onStart:function(){
+            $('#second-lowest').textillate({
+                    in: { 
+                        effect: 'fadeInUp',
+                        callback:function(){
+                            $('#second-lowest').textillate('out')
+                                }
+                        },
+                    out: { effect: 'fadeOutUp' } 
+                });
+                }})
+        
+        .from("#second-top",{
+            opacity:0,
+            delay:1,
+    
+            onStart:function(){
+                $('#second-top').textillate({
+                        in: { 
+                            effect: 'fadeInUp',
+                            callback:function(){
+                                $('#second-top').textillate('out')
+                                    }
+                            },
+                        out: { effect: 'fadeOutUp' } 
+                    });
+                    }})
+    
+        .from("#top",{
+            opacity:0,
+            delay:1.2,
+            onStart:function(){
+                $('#top').textillate({
+                        in: { 
+                            effect: 'fadeInUp',
+                            callback:function(){
+                                $('#top').textillate('out')
+                                    }
+                            },
+                        out: { effect: 'fadeOutUp'}
+                    });
+                    }})
+    
+        .to("#top-screen",{
+            top: "-120%",
+            delay: 1,
+            duration:1,
+            ease:"Power4.easeOut",
+        })
+    
+    
+    
+    
+    
+    var  tl= gsap.timeline();
+    tl
+        .from(".navbar",{
+            opacity: 0,
+            y:-40,
+            duration:0.5,
+        })
+    
+        .from(".hero-head h1, p",{
+            opacity:0,
+            color:"grey",
+            scale: 1.5,
+            duration:0.9,
+            delay:0.5
+        })
+        .from(".hero-container button",{
+            scale:0.9,
+            repeat: -1,
+            yoyo: "true",
+            opacity:1,
+            duration:1
+        })
+        .from(".hero-container img",{
+            y:150,
+            opacity:0,
+            duration:1
+        })
+    
